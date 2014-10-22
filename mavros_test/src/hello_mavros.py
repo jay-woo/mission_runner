4bb84b83e4a6f789eb300a1ac91011a8c90e6347
Eric Schneider <franz.eric.schneider@gmail.com>
2014-10-22 10:31:40 -0400

    Adding waypoints to mavros test

--- mavros_test/src/hello_mavros.py
+++ mavros_test/src/hello_mavros.py
@@ -8,11 +8,14 @@ from mavros.msg import *
 from mavros.srv import *
 
 
+# TODO: Change given altitude and observe results
+# TODO: Change pitch/yaw and see the results
 def launch(latitude, longitude, min_pitch = 0, yaw = 0, altitude = 4):
-    topic = '/mavros/cmd/takeoff'
-    # rospy.wait_for_service(topic)
+    service = '/mavros/cmd/takeoff'
+    rospy.wait_for_service(service)
+    rospy.loginfo('Succesfully found service %s', service)
     try:
-        command = rospy.ServiceProxy(topic, CommandTOL)
+        command = rospy.ServiceProxy(service, CommandTOL)
         res = command(min_pitch, yaw, latitude, longitude, altitude)
         print 'res: ',res
         if str(res) == 'success: True':
@@ -21,10 +24,29 @@ def launch(latitude, longitude, min_pitch = 0, yaw = 0, altitude = 4):
         else:
             print 'error launching'
             return False
-        except rospy.ServiceException, e:
-            rospy.logwarn('Error encountered: %s', str(e))
+    except rospy.ServiceException, e:
+        rospy.logwarn('Error encountered: %s', str(e))
+        return False
+        rospy.loginfo('Ran launch')
+
+def land(latitude, longitude, min_pitch = 0, yaw = 0, altitude = 0):
+    service = '/mavros/cmd/land'
+    rospy.wait_for_service(service)
+    rospy.loginfo('Succesfully found service %s', service)
+    try:
+        command = rospy.ServiceProxy(service, CommandTOL)
+        res = command(min_pitch, yaw, latitude, longitude, altitude)
+        print 'res: ',res
+        if str(res) == 'success: True':
+            print 'successfully landing'
+            return True
+        else:
+            print 'error landing'
             return False
-            rospy.loginfo('Ran launch')
+    except rospy.ServiceException, e:
+        rospy.logwarn('Error encountered: %s', str(e))
+        return False
+        rospy.loginfo('Ran land')
 
 # TEST Remove this function if it works
 def callback(msg):
@@ -38,9 +60,10 @@ def listener():
     rospy.loginfo('Just subscribed to %s', topic)
     rospy.spin()
     rospy.loginfo('Ran listener')
-    if __name__ == '__main__':
-        rospy.init_node('listener', anonymous=True)
-        lat = 42.292829
-        lon = -71.263084
-        launch(lat, lon)
-        listener()
+
+if __name__ == '__main__':
+    rospy.init_node('listener', anonymous=True)
+    lat = 42.292861
+    lon = -71.263073
+    launch(lat, lon)
+    listener()
